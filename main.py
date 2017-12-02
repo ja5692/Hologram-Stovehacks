cb7850887d41da2fc2ae8e13fa4aaa7ee586691d
Jonathan Xu <jonathanlikestoemail@gmail.com>
2017-12-02 18:25:11 -0500

    :')

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from Hologram.HologramCloud import HologramCloud
 import json
 import threading
-from msvcrt import getch
 
 #Instantiating a hologram instance
 hologram = HologramCloud(dict(), network='cellular')
