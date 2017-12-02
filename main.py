fe6ab07bf1051a26c0795bafc888d7200ca85dfc
Jonathan Xu <jonathanlikestoemail@gmail.com>
2017-12-02 15:35:53 -0500

    Raspberry Main code

---
+++ main.py
@@ -0,0 +1,11 @@
+from Hologram.HologramCloud import HologramCloud
+
+//Instantiating a hologram instance
+hologram = HologramCloud(dict(), network='cellular')
+
+result = hologram.network.connect()
+if result == False:
+    print ' Failed to connect to cell network'
+
+response_code = hologram.sendMessage("hello, world!")
+print hologram.getResultString(response_code)
