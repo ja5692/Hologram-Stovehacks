5c098a49694929c52b2151586280fee72bc13a14
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-01-17 22:33:21 -0500

    temperature only

--- main.py
+++ main.py
@@ -17,14 +17,10 @@ def update():
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
     temperature = '{0:0.1f}'.format(temperature)
-    humidity = '{0:0.1f}'.format(humidity)
-    print "humid:"
-    print humidity
-    print "temp:"
-    print temperature
 
     #Upload Data
-    hologram.sendMessage(json.dumps({"Humidity":Humidity, "Temperature":temperature}))
+    hologram.sendMessage(json.dumps({"Temperature":temperature + "C"}))
+
 
 update()
 hologram.network.disconnect()
