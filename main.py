31e41ab00825d4bea9f0af156f6e77c4451747e3
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-01-17 21:27:02 -0500

    dht

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Hologram.HologramCloud import HologramCloud
 import json
 import threading
 import time
+import sys
+import Adafruit_DHT
 
 #Instantiating a hologram instance
 hologram = HologramCloud(dict(), network='cellular')
@@ -25,6 +27,9 @@ def update():
     print location.date
     print "Time:"
     print location.time
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
 
     #Upload Databa
     hologram.sendMessage(json.dumps({"Altitude":location.altitude, "Longitude":location.longitude, "Latitude":location.latitude, "Uncertainty": location.uncertainty, "Date":location.date, "Time":location.time}))
