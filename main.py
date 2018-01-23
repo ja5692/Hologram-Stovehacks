5a15a6a180c864254439416eefc6a92903b069ac
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-01-22 19:51:54 -0500

    valueconversion

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ recv = hologram.enableSMS()
 def update():
     #Log temperature
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    temperature = '{0:0.1f}'.format(temperature)
+    temperature = float('{0:0.1f}'.format(temperature))
     print "Variable type: " + str(type(temperature))
 
     #Determine if stove is on or off
@@ -35,23 +35,24 @@ def update():
         while True:
             #Reads user response
             sms_obj = hologram.popReceivedSMS()
-            message = sms_obj.message
-
-            if message == "yes":
-                #hologram.sendMessage(json.dumps("Turning off stove."))
-                print "Turning off stove."
-                break
-            elif message == "no":
-                #hologram.sendMessage(json.dumps("Ok. Stove will be left on."))
-                print "Ok. Stove will be left on."
-                break
-            elif message:
-                #hologram.sendMessage(json.dumps("Please enter a valid response. (yes/no)"))
-                print "Please enter a valid response. (yes/no)"
-            elif count >= 30:
-                #hologram.sendMessage(json.dumps("No response recieved within 30 minutes. Reverting to standby mode."))
-                print "No response recieved within 30 minutes. Reverting to standby mode."
-                break
+            if sms_obj is not None:
+                message = sms_obj.message
+                if message == "yes":
+                    #hologram.sendMessage(json.dumps("Turning off stove."))
+                    print "Turning off stove."
+                    break
+                elif message == "no":
+                    #hologram.sendMessage(json.dumps("Ok. Stove will be left on."))
+                    print "Ok. Stove will be left on."
+                    break
+                elif message:
+                    #hologram.sendMessage(json.dumps("Please enter a valid response. (yes/no)"))
+                    print "Please enter a valid response. (yes/no)"
+                    count = 0
+                elif count >= 30:
+                    #hologram.sendMessage(json.dumps("No response recieved within 30 minutes. Reverting to standby mode."))
+                    print "No response recieved within 30 seconds. Reverting to standby mode."
+                    break
             count += 1
             time.sleep(1)
 
